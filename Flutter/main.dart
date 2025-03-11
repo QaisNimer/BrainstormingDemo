b53a9fa4b6f7fefc5982dddb2831d72cbdd9e710
alaa21as <alaa.srour0032@gmail.com>
2025-03-12 02:02:09 +0300

    Add files via upload

--- Flutter/main.dart
+++ Flutter/main.dart
@@ -1,35 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:foodtek/view/screens/splash_screen.dart';
-import 'package:provider/provider.dart';
+import 'package:untitled/view/screen/otp_screen.dart';
 
-import 'controller/login_controller.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => LoginController(),
-        ),
-
-      ],
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: SplashScreen(),
-      ),
+    return MaterialApp(
+      home: OTPScreen(),
     );
   }
 }
-
